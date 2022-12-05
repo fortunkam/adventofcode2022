f6a767d60b6616ce244afb7b291cb1b23aae9cb7
Matthew Fortunka <matthew@fortunka.co.uk>
2022-12-05 11:20:58 +0000

    added day 5 part 2

--- day5/Cargo.cs
+++ day5/Cargo.cs
@@ -19,7 +19,7 @@ public static class Cargo
                 var stackLine = sr.ReadLine();
                 do
                 {
-                    Console.WriteLine($"add line {stackLine}");
+                    //Console.WriteLine($"add line {stackLine}");
                     lines.Add(stackLine);
                     stackLine = sr.ReadLine();
                 }
@@ -34,7 +34,7 @@ public static class Cargo
                     stacks.Add(new List<char>());
                 }
 
-                Console.WriteLine($"Last line {last} shows a count of {count}");
+                //Console.WriteLine($"Last line {last} shows a count of {count}");
 
                 var regexStackItem = new Regex(@"(\s{3}|\[(?<m>[A-Z])\])[\s|$]?", RegexOptions.Compiled);
                 
@@ -48,7 +48,7 @@ public static class Cargo
                         var cmatch = rmatch[j];
                         if(!string.IsNullOrWhiteSpace(cmatch.Value))
                         {
-                            Console.WriteLine($"Inserting {cmatch.Groups["m"].Value.ToCharArray().First()} into column {j}");
+                            //Console.WriteLine($"Inserting {cmatch.Groups["m"].Value.ToCharArray().First()} into column {j}");
                             stacks[j].Insert(0,cmatch.Groups["m"].Value.ToCharArray().First());
                         }
                     }
@@ -90,6 +90,96 @@ public static class Cargo
 
     public static void PartTwo(string inputFile)
     {
+        using var fs = new FileStream(inputFile, FileMode.Open);
+        using var sr = new StreamReader(fs);
 
+        var stacks = new List<List<char>>();
+
+        bool processingStacks = true;
+        var moves = new Regex(@"move (?<num>\d*) from (?<from>\d*) to (?<to>\d*)", RegexOptions.Compiled);
+        do
+        {
+            
+            if(processingStacks)
+            {
+                var lines = new List<string>();
+                var stackLine = sr.ReadLine();
+                do
+                {
+                    //Console.WriteLine($"add line {stackLine}");
+                    lines.Add(stackLine);
+                    stackLine = sr.ReadLine();
+                }
+                while(!string.IsNullOrWhiteSpace(stackLine));
+
+                // Last line contains the number of columns
+
+                var last = lines[lines.Count - 1];
+                var count = int.Parse(last.Trim().Split(' ').Last());
+                for(var j=0; j < count; j++)
+                {
+                    stacks.Add(new List<char>());
+                }
+
+               // Console.WriteLine($"Last line {last} shows a count of {count}");
+
+                var regexStackItem = new Regex(@"(\s{3}|\[(?<m>[A-Z])\])[\s|$]?", RegexOptions.Compiled);
+                
+                
+                for (var i = 0; i < (lines.Count - 1); i++)
+                {
+                    var curLine = lines[i];
+                    var rmatch = regexStackItem.Matches(curLine);
+                    for(var j=0; j < count; j++)
+                    {
+                        var cmatch = rmatch[j];
+                        if(!string.IsNullOrWhiteSpace(cmatch.Value))
+                        {
+                            //Console.WriteLine($"Inserting {cmatch.Groups["m"].Value.ToCharArray().First()} into column {j}");
+                            stacks[j].Insert(0,cmatch.Groups["m"].Value.ToCharArray().First());
+                        }
+                    }
+
+                }
+
+                processingStacks = false;
+            }
+            else
+            {
+                var line = sr.ReadLine();
+                var match = moves.Match(line);
+                if (match.Success)
+                {
+                    var num = int.Parse(match.Groups["num"].Value);
+                    var from = int.Parse(match.Groups["from"].Value) - 1;
+                    var to = int.Parse(match.Groups["to"].Value) - 1;
+
+
+                    //Grab the last n values 
+                    var valsToInsert = stacks[from].Skip(stacks[from].Count - num).Take(num).ToArray();
+                    DebugStack(nameof(valsToInsert), valsToInsert);
+
+                    stacks[from].RemoveRange(stacks[from].Count - num, num);
+
+                    stacks[to].AddRange(valsToInsert);
+                }
+            }            
+        }
+        while(!sr.EndOfStream);
+        
+        var result = "";
+        foreach(var item in stacks)
+        {
+            result += item.Last().ToString();
+        }
+
+        Console.WriteLine(result);
+
+    }
+
+    private static void DebugStack(string stackidentifier, IEnumerable<char> vals)
+    {
+        var joined = string.Join(",", vals);
+        Console.WriteLine($"{stackidentifier}: {joined}");
     }
 }
