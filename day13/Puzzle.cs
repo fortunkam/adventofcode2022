73d064ab9e05082678b4f558d294636f89359bb6
fortunkam <matthew@fortunka.co.uk>
2022-12-13 17:19:18 +0000

    added more day13 WIP

--- day13/Puzzle.cs
+++ day13/Puzzle.cs
@@ -5,18 +5,97 @@ public static class Puzzle
 {
     public static void PartOne(string inputFile)
     {
+
+        var ed = ElementFactory.FromLine("[[1],[2,3,4]]");
+        Console.WriteLine(ed.ToString());
+
         using var fs = new FileStream(inputFile, FileMode.Open);
         using var sr = new StreamReader(fs);
+
+        Element first = null;
+        Element second = null;
+
+        var validIndicies = new List<int>();
+        var currentPair = 1;
 
         do
         {  
             var line = sr.ReadLine();
-            if(string.IsNullOrWhiteSpace(line)) continue;
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                if (AreEqual(first, second))
+                {
+                    validIndicies.Add(currentPair);
+                }
 
-            var element = ElementFactory.FromLine(line);
+                first = null;
+                second = null;
+                currentPair++;
+
+            }
+            else
+            {
+                var element = ElementFactory.FromLine(line);
+                if (first == null)
+                {
+                    first = element;
+                }
+                else
+                {
+                    second = element;
+                }
+            }
 
         }
         while (!sr.EndOfStream);
+
+        foreach(var i in validIndicies)
+        {
+            Console.WriteLine($"{i} is valid");
+        }
+
+        Console.WriteLine(validIndicies.Aggregate((r1, r2) => r1 * r2));
+    }
+
+    public static bool AreEqual(Element one, Element two)
+    {
+        if(one is IntElement && two is IntElement)
+        {
+            return (one as IntElement).Value <= (one as IntElement).Value;       
+        }
+
+        if (one is IntElement && two is ArrayElement)
+        {
+            var tmpOne = new ArrayElement();
+            tmpOne.Elements.Add(one);
+            return AreEqual(tmpOne, two);
+        }
+
+        if (one is ArrayElement && two is IntElement)
+        {
+            var tmpTwo = new ArrayElement();
+            tmpTwo.Elements.Add(two);
+            return AreEqual(one, tmpTwo);
+        }
+
+        if (one is ArrayElement && two is ArrayElement)
+        {
+            var result = true;
+            var a1 = one as ArrayElement;
+            var a2 = two as ArrayElement;
+
+            if (a2.Elements.Count >= a1.Elements.Count) return false;
+
+            for (int i = 0; i < a1.Elements.Count; i++)
+            {
+                if (i >= a2.Elements.Count) break;
+                result = result && AreEqual(a1.Elements[i], a2.Elements[i]);
+            }
+            return result;
+
+        }
+
+         return false;
     }
 
     public static void PartTwo(string inputFile)
@@ -30,6 +109,8 @@ public static class Puzzle
         while (!sr.EndOfStream);
     }
 }
+
+
 
 
 public abstract class Element
@@ -49,14 +130,14 @@ public class IntElement : Element
 
 public class ArrayElement : Element
 {
-    public List<Element> Elements {get;set;}
+    public List<Element> Elements {get;set;} = new List<Element>();
 
     public override string ToString()
     {
-        var sb = new StringBuilder("ARRAY");
+        var sb = new StringBuilder("ARRAY\r\n");
         foreach(var e in Elements)
         {
-            sb.Append($"\t{e.ToString()}");
+            sb.Append($"\t{e.ToString()}\r\n");
         }
         return sb.ToString();
     }
@@ -68,16 +149,16 @@ public static class ElementFactory
     private static Regex matchArray = new Regex(@"(?<start>\[)(?<all>.*?)(?<end>\])", RegexOptions.Compiled);
     public static Element FromLine(string line)
     {
-        Console.WriteLine($"Processing {line}");
+       // Console.WriteLine($"Processing {line}");
         if(matchInt.IsMatch(line)) 
         {
-            Console.WriteLine($"Found Integer {line}");
+           // Console.WriteLine($"Found Integer {line}");
             return new IntElement{ Value = int.Parse(line)};
         }
 
         var startArray = new ArrayElement();
         ArrayElement currentArray = null;
-        var arrays = new Queue<ArrayElement>();
+        var arrays = new Stack<ArrayElement>();
         var numbers = new List<IntElement>();
 
         var buffer = new List<char>();
@@ -86,7 +167,7 @@ public static class ElementFactory
             switch(i)
             {
                 case ']':
-                    var curr = arrays.Dequeue();
+                    var curr = arrays.Pop();
                     if(buffer.Count > 0)
                     {
                         var number = int.Parse(new String(buffer.ToArray()));
@@ -108,14 +189,27 @@ public static class ElementFactory
                     if(currentArray == null)
                     {
                         currentArray = startArray;
+
+                        if (numbers.Count > 0)
+                        {
+                            currentArray.Elements.AddRange(numbers);
+                            numbers.Clear();
+                        }
                     }
                     else
                     {
                         var newArray = new ArrayElement();
                         currentArray.Elements.Add(newArray);
+
+                        if (numbers.Count > 0)
+                        {
+                            currentArray.Elements.AddRange(numbers);
+                            numbers.Clear();
+                        }
+
                         currentArray = newArray;
                     }
-                    arrays.Enqueue(currentArray);
+                    arrays.Push(currentArray);
                 break;
                 default:
                     buffer.Insert(0,i);
