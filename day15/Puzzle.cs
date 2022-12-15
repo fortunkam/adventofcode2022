a5b2abcfbcca9c506c9cac995d305b8ee3c44315
fortunkam <matthew@fortunka.co.uk>
2022-12-15 14:17:59 +0000

    part 1 works for test not real

--- day15/Puzzle.cs
+++ day15/Puzzle.cs
@@ -24,11 +24,11 @@ public abstract class Element
 
 public class Sensor : Element
 {
-    private readonly Beacon _closestBeacon;
 
-    public Sensor(int x, int y, Beacon closestBeacon) : base(x, y, "S", ConsoleColor.Yellow)
+    public Sensor(int x, int y, Beacon closestBeacon, int targetRow) : base(x, y, "S", ConsoleColor.Yellow)
     {
-        _closestBeacon = closestBeacon;
+        ClosestBeacon = closestBeacon;
+        this._targetRow = targetRow;
         CalculateNoBeacons();
     }
 
@@ -37,22 +37,24 @@ public class Sensor : Element
         //Distance to beacon
         _noBeacons.Clear();
 
-        var diffX = Math.Abs(this.X - _closestBeacon.X);
-        var diffY = Math.Abs(this.Y - _closestBeacon.Y);
+        var diffX = Math.Abs(this.X - ClosestBeacon.X);
+        var diffY = Math.Abs(this.Y - ClosestBeacon.Y);
 
         var distance = diffX + diffY;
 
         var itemCount = 1;
-        var rightItemCount = 1;
+
         for (var y = this.Y - distance; y <= this.Y + distance; y++)
         {
-            _noBeacons.Add(new Empty(this.X, y));
-            for (int i = 1; i < itemCount; i++)
+            if (y == _targetRow)
             {
-                _noBeacons.Add(new Empty(this.X - i, y));
-                _noBeacons.Add(new Empty(this.X + i, y));
+                _noBeacons.Add(new Empty(this.X, y));
+                for (int i = 1; i < itemCount; i++)
+                {
+                    _noBeacons.Add(new Empty(this.X - i, y));
+                    _noBeacons.Add(new Empty(this.X + i, y));
+                }
             }
-
             if (y > this.Y)
             {
                 itemCount--;
@@ -63,26 +65,45 @@ public class Sensor : Element
             }
         }
 
-        _noBeacons.RemoveAll(x => x.X == _closestBeacon.X && x.Y == _closestBeacon.Y);
+        _noBeacons.RemoveAll(x => x.X == ClosestBeacon.X && x.Y == ClosestBeacon.Y);
 
 
 
     }
 
-    public static Sensor FromLine(string line)
+    public static Sensor FromLine(string line, int targetRow)
     {
         var match = Regex.Match(line, @"Sensor at x=(?<sx>-*\d*), y=(?<sy>-*\d*): closest beacon is at x=(?<bx>-*\d*), y=(?<by>-*\d*)");
         return new Sensor(int.Parse(match.Groups["sx"].Value), int.Parse(match.Groups["sy"].Value),
-            new Beacon(int.Parse(match.Groups["bx"].Value), int.Parse(match.Groups["by"].Value)));
+            new Beacon(int.Parse(match.Groups["bx"].Value), int.Parse(match.Groups["by"].Value)), targetRow);
     }
 
+    public Beacon ClosestBeacon { get; }
+
     private List<Empty> _noBeacons = new List<Empty>();
+    private readonly int _targetRow;
 
     public Empty[] NoBeacons
     {
         get
         {
             return _noBeacons.ToArray();
+        }
+    }
+
+    public int RangeMinX
+    {
+        get
+        {
+            return _noBeacons.Count > 0 ? _noBeacons.Min(r => r.X) : int.MaxValue;
+        }
+    }
+
+    public int RangeMaxX
+    {
+        get
+        {
+            return _noBeacons.Count > 0 ? _noBeacons.Max(r => r.X): int.MinValue;
         }
     }
 }
@@ -104,7 +125,7 @@ public class Beacon : Element
 public static class Puzzle
 {
 
-    public static List<Sensor> GetSensors(string inputFile)
+    public static List<Sensor> GetSensors(string inputFile, int targetRow)
     {
         using var fs = new FileStream(inputFile, FileMode.Open);
         using var sr = new StreamReader(fs);
@@ -113,7 +134,7 @@ public static class Puzzle
         do
         {
             var line = sr.ReadLine();
-            sensorList.Add(Sensor.FromLine(line));
+            sensorList.Add(Sensor.FromLine(line, targetRow));
             
         }
         while (!sr.EndOfStream);
@@ -123,15 +144,37 @@ public static class Puzzle
     
     public static void PartOne(string inputFile)
     {
-            // var sensors = Puzzle.GetSensors(inputFile);
+        var targetRow = 10;
+        var sensors = Puzzle.GetSensors(inputFile, targetRow);
 
-            var testSensor = Sensor.FromLine("Sensor at x=8, y=7: closest beacon is at x=2, y=10");
-            Console.WriteLine(testSensor);
+        var minX = sensors.Min(r => r.RangeMinX);
+        var maxX = sensors.Max(r => r.RangeMaxX);
+
+        
+
+        var emptyYPoints = new Dictionary<int, int>(Enumerable.Range(minX, maxX - minX + 1).Select(r => new KeyValuePair<int, int>(r, 0)));
+        foreach(var sensor in sensors)
+        {
+            var noHits = sensor.NoBeacons.Where(r => r.Y == targetRow).Select(r => r.X);
+            foreach(var n in noHits)
+            {
+                emptyYPoints[n]++;
+            }
+        }
+
+
+
+
+
+
+
+        //var testSensor = Sensor.FromLine("Sensor at x=8, y=7: closest beacon is at x=2, y=10");
+        Console.WriteLine(emptyYPoints.Where(r=>r.Value != 0).Count());
 
     }
 
     public static void PartTwo(string inputFile)
     {
-        var sensors = Puzzle.GetSensors(inputFile);
+        var sensors = Puzzle.GetSensors(inputFile, 0);
     }
 }
